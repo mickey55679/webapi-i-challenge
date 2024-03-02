839594619acfe2e5a4c708ee4137773cce66ab9e
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-01 20:33:50 -0600

    wip

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-// implement your API here
-// require the express npm module, needs to be added to the project using "npm install express"
 const express = require("express");
-const data = require("./data/db");
 
-// creates an express application using the express module
 const server = express();
+server.use(express.json()); // !! IMPORTANT this teaches express to parse req.body
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
-server.get("/", (req, res) => {
-  // express will pass the request and response objects to this function
-  // the .send() on the response object can be used to send a response to the client
-  res.send("Hello World");
-});
+let id = 0;
+let getId = () => ++id; // helper function to create auto-incrementing ids
+
+let hobbits = [
+  // our fake hobbits database table
+  { id: getId(), name: "Samwise Gamgee" },
+  { id: getId(), name: "Frodo Baggins" },
+];
+
 server.get("/hobbits", (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: "Samwise Gamgee",
-    },
-    {
-      id: 2,
-      name: "Frodo Baggins",
-    },
-  ];
+  // GET ALL EXISTING HOBBITS
+  res.status(200).json(hobbits); // 200 means "OK"
+});
+server.get("/hobbits/:id", (req, res) => {
+  // GET EXISTING HOBBIT BY id
+  // the desired id comes in the URL, and is found in `req.params.id`
+  res.status(200).json(hobbits.find((hob) => hob.id == req.params.id));
+});
+server.post("/hobbits", (req, res) => {
+  // POST NEW HOBBIT
+  // the desired name comes in the body, and is found in `req.body.name`
+  hobbits.push({ id: getId(), name: req.body.name });
+  res.status(201).json(hobbits); // 201 means "Created"
+});
+server.put("/hobbits/:id", (req, res) => {
+  // PUT EXISTING HOBBIT
+  // the id to update is in `req.params.id` and the desired name in `req.body.name`
+  hobbits = hobbits.map((hob) =>
+    hob.id == req.params.id ? { ...hob, name: req.body.name } : hob
+  );
   res.status(200).json(hobbits);
 });
-server.get("/users", (req, res) => {
-  data
-    .find()
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .json({ error: "The users information could not be retrieved." });
-    });
+server.delete("/hobbits/:id", (req, res) => {
+  // DELETE EXISTING HOBBIT
+  hobbits = hobbits.filter((hob) => hob.id != req.params.id);
+  res.status(200).json(hobbits);
 });
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
 server.listen(8000, () => console.log("API running on port 8000"));
